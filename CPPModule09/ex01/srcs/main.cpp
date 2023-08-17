699083bc59ef0d63f72c47ca5e742c861c59fcac
Sean Chuah <chuahtseyung2002@gmail.com>
2023-08-17 18:24:27 +0800

    Add return

--- CPPModule09/ex01/srcs/main.cpp
+++ CPPModule09/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:15:45 by schuah            #+#    #+#             */
-/*   Updated: 2023/04/04 21:08:50 by schuah           ###   ########.fr       */
+/*   Updated: 2023/08/17 18:22:52 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,27 @@ int main(int ac, char **av)
 	if (ac != 2)
 	{
 		std::cout << av[0] << " [expression]" << std::endl;
-		return ;
+		return (1);
 	}
 
-    std::stack<char>	stack;
-	std::stringstream	ss(av[1]);
-	int					error = 0;
-	char				token;
+	std::stack<char> stack;
+	std::stringstream ss(av[1]);
+	int error = 0;
+	char token;
 	while (ss >> token && error == 0)
 	{
 		if (stack.size() < 2)
 			(token >= '0' && token <= '9') ? stack.push(token) : (void)(error = 1);
 		else
 		{
-			std::string	ops = "+-*/";
+			std::string ops = "+-*/";
 			if (ops.find(token) == std::string::npos)
 				error = 1;
 			else
 			{
-				int	b = stack.top() - '0';
+				int b = stack.top() - '0';
 				stack.pop();
-				int	a = stack.top() - '0';
+				int a = stack.top() - '0';
 				stack.pop();
 				if (token == '+')
 					stack.push(a + b + '0');
@@ -53,4 +53,5 @@ int main(int ac, char **av)
 		}
 	}
 	std::cout << ((error || stack.size() != 1) ? "Error" : std::to_string(stack.top() - '0')) << std::endl;
+	return (0);
 }
